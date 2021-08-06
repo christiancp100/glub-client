2c0351c25462044c0a772054dd6a51f71cd9c119
Christian Capeans <christiancp100@gmail.com>
2021-08-07 00:11:58 +0200

    Prepare the environment to develop

---
+++ src/components/CustomText.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text, StyleSheet } from 'react-native'
+
+export const CustomText = ({ children }) => {
+  return <Text>{children}</Text>
+}
+
+export default CustomText
